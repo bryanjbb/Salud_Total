c8270713f62dcc10f3a483dc9b5206655766e558
bryanbermudez <bryan06jb@gmail.com>
2024-10-25 04:17:53 -0600

    PenultimoTablas catálogo

---
+++ Salud Total/Models/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Salud_Total.Models
+{
+    public class Venta
+    {
+        [Key]
+        public int N_Factura { get; set; }
+
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+    }
+}
+
+
